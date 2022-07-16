075870d696976a72bffac500d06d042420bb8968
DAIKI URATA <7nohe.urata@gmail.com>
2022-07-16 23:51:12 +0900

    fix code by eslint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import type { Plugin } from "vite";
-import { parse } from "@vue/compiler-sfc";
-import { marked } from "marked";
-import { parse as _parse } from "@babel/parser";
+import type { Plugin } from 'vite'
+import { parse } from '@vue/compiler-sfc'
+import { marked } from 'marked'
+import { parse as _parse } from '@babel/parser'
 
-const ID = "vmarked";
+const ID = 'vmarked'
 
-type PluginOptions = {
-  options?: marked.MarkedOptions;
-  extensions?: marked.MarkedExtension[];
-};
+interface PluginOptions {
+  options?: marked.MarkedOptions
+  extensions?: marked.MarkedExtension[]
+}
 
 const initMarked = ({ options, extensions }: PluginOptions) => {
-  if (options) {
-    marked.setOptions(options);
-  }
-  if (extensions) {
-    marked.use(...extensions);
-  }
-};
+  if (options)
+    marked.setOptions(options)
+
+  if (extensions)
+    marked.use(...extensions)
+}
 
 const replaceCode = (
   {
@@ -26,9 +25,9 @@ const replaceCode = (
     variableName,
   }: { start: number; end: number; variableName: string },
   code: string,
-  markdwon: string
+  markdwon: string,
 ): string => {
-  const html = marked(markdwon);
+  const html = marked(markdwon)
   const comp = `
   import {h, defineComponent} from "vue";
   let _comp = defineComponent({
@@ -44,76 +43,77 @@ const replaceCode = (
   
   _comp.render = _render;
   const ${variableName} = _comp;
-  `;
-  return `${code.slice(0, start)} \n ${comp} \n ${code.slice(end)}`;
-};
+  `
+  return `${code.slice(0, start)} \n ${comp} \n ${code.slice(end)}`
+}
 
 function vitePluginVueMarked(
-  options: PluginOptions = { options: {}, extensions: [] }
+  options: PluginOptions = { options: {}, extensions: [] },
 ): Plugin {
   return {
-    name: "vite-plugin-vue-marked",
-    enforce: "pre",
+    name: 'vite-plugin-vue-marked',
+    enforce: 'pre',
     resolveId(id) {
-      if (id === ID) return ID;
+      if (id === ID)
+        return ID
     },
     async transform(code: string, id: string) {
-      const fileRegex = /\.(vue)$/;
+      const fileRegex = /\.(vue)$/
 
       // Check if file is a vue file
       if (fileRegex.test(id)) {
-        const { descriptor } = parse(code);
+        const { descriptor } = parse(code)
         if (
-          descriptor.scriptSetup &&
-          descriptor.customBlocks.find((b) => b.type === "markdown")
+          descriptor.scriptSetup
+          && descriptor.customBlocks.find(b => b.type === 'markdown')
         ) {
-          initMarked(options);
+          initMarked(options)
           const markdwon = descriptor.customBlocks.find(
-            (b) => b.type === "markdown"
-          )!.content;
-          const { content } = descriptor.scriptSetup;
+            b => b.type === 'markdown',
+          )!.content
+          const { content } = descriptor.scriptSetup
           const nodes = _parse(content, {
-            sourceType: "module",
-            plugins: ["typescript"],
-          }).program.body;
+            sourceType: 'module',
+            plugins: ['typescript'],
+          }).program.body
 
           // Find the script node
           nodes.forEach((node) => {
-            if (node.type === "ImportDeclaration" && node.source.value === ID) {
+            if (node.type === 'ImportDeclaration' && node.source.value === ID) {
               const defaultSpecifier = node.specifiers.find(
-                (specifier) => specifier.type === "ImportDefaultSpecifier"
-              );
+                specifier => specifier.type === 'ImportDefaultSpecifier',
+              )
 
               const replacement = replaceCode(
                 {
                   start: node.start!,
                   end: node.end!,
-                  variableName: defaultSpecifier?.local?.name ?? "",
+                  variableName: defaultSpecifier?.local?.name ?? '',
                 },
                 content,
-                markdwon
-              );
-              code = code.replace(content, replacement);
+                markdwon,
+              )
+              code = code.replace(content, replacement)
             }
-          });
+          })
 
           // Remove custom block
-          const start = code.indexOf("<markdown>");
-          const end = code.indexOf("</markdown>");
+          const start = code.indexOf('<markdown>')
+          const end = code.indexOf('</markdown>')
           const customBlockContent = code.slice(
             start,
-            end + "</markdwon>".length
-          );
-          code = code.replace(customBlockContent, "");
+            end + '</markdwon>'.length,
+          )
+          code = code.replace(customBlockContent, '')
         }
       }
 
       return {
         code,
         map: null,
-      };
+      }
     },
-  };
+  }
 }
 
-export default vitePluginVueMarked;
+export default vitePluginVueMarked
